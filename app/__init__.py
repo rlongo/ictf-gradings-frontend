47b7ac09749228a123dabc215af5cac4854d083d
robert longo <robertflongo@gmail.com>
2019-09-02 16:02:11 -0400

    Fixed: error handling in the server

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 #!/bin/python3
 
+import tempfile
+
 from flask import Flask, render_template, request, send_file
 from datetime import datetime
 from .processing import run_pipeline
@@ -41,21 +43,27 @@ def form():
         keys = ['group_authority', 'kjn', 'dojang',
                 'head_instructor', 'admin2', 'admin3',
                 'date_year', 'date_month', 'date_day']
-        for k in keys:
-            config[k] = request.form.get(k)
 
-        request_file = request.files['student_list']
-        if not request_file:
-            return "No file"
+        try:
+            for k in keys:
+                config[k] = request.form.get(k)
+                if len(config[k]) is 0:
+                    raise Exception("Failed to specify value for: '%s'" % k)
 
-        file_contents = request_file.stream
+            request_file = request.files['student_list']
+            if not request_file:
+                raise Exception("No file submitted containing student data")
+           
+            scratch_dir = tempfile.mkdtemp()
+            file_contents = request_file.stream
+            output = run_pipeline(scratch_dir, file_contents, config)
+            return send_file(output)
+        except BaseException as e:
+            config['error_message'] = str(e)
 
-        output = run_pipeline('/tmp/scratch/ictf/', file_contents, config)
-        return send_file(output)
-    else:
-        config['date_year'], config['date_month'], config['date_day'] = get_time()
-        config['group_authority'] = 'WOODBRIDGE TAEKWON-DO GROUP - GRAND MASTER J. CARIATI IX DAN'
-        config['kjn'] = 'GRAND MASTER J. CARIATI IX DAN'
-        config['dojang'] = 'Woodbridge Taekwondo'
+    config['date_year'], config['date_month'], config['date_day'] = get_time()
+    config['group_authority'] = 'WOODBRIDGE TAEKWON-DO GROUP - GRAND MASTER J. CARIATI IX DAN'
+    config['kjn'] = 'GRAND MASTER J. CARIATI IX DAN'
+    config['dojang'] = 'Woodbridge Taekwondo'
 
     return render_template('form.html', **config)
